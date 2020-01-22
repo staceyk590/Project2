d4347869e78f1d604436628f63bada0c6cdb79f0
Stacey Koumaras <stacey_k@tpg.com.au>
2020-01-22 21:18:27 +1100

    merged

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+/* dummy data for author table */
+
+INSERT INTO author (author_name) VALUES ('khoi');
+INSERT INTO author (author_name) VALUES ('andrew');
+INSERT INTO author (author_name) VALUES ('stacey');
+INSERT INTO author (author_name) VALUES ('hiru');
+
+/* dummy data for hotels table */
+
+INSERT INTO hotels (hotel_name,hotel_address, hotel_city, review_text, hotel_ratings, user_id) VALUES ('Rydges', 'Mascot', '8 Arrivals Court Sydney Airport', 'Always stay here for international flight and never been dissapointed', 5 , 1);
+INSERT INTO hotels (hotel_name,hotel_address, hotel_city, review_text, hotel_ratings, user_id) VALUES ();
+INSERT INTO hotels (hotel_name,hotel_address, hotel_city, review_text, hotel_ratings, user_id) VALUES ();
+INSERT INTO hotels (hotel_name,hotel_address, hotel_city, review_text, hotel_ratings, user_id) VALUES ();
+INSERT INTO hotels (hotel_name,hotel_address, hotel_city, review_text, hotel_ratings, user_id) VALUES ();
+
+
+
