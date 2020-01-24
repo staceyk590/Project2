5ac546672d3b09ca4f72982911a781588acd6248
Stacey Koumaras <stacey_k@tpg.com.au>
2020-01-25 10:33:46 +1100

    merged

---
+++ js/add.js
@@ -0,0 +1,36 @@
+// Code here handles what happens when a user submits a new character on the form.
+// Effectively it takes the form inputs then sends it to the server to save in the DB.
+
+// when user clicks add-btn
+$("#add-btn").on("click", function(event) {
+  event.preventDefault();
+
+  // make a newReview obj
+  var newReview = {
+    // city from city input
+    City: $("#city").val().trim(),
+    // hotel from hotel input
+    Hotel: $("#hotel").val().trim(),
+    // ratings from ratings input
+    Ratings: $("#rating").val().trim(),
+    // comments from comments input
+    Comments: $("#comments").val().trim()
+  };
+
+  // send an AJAX POST-request with jQuery
+  $.post("/api/new", newReview)
+    // on success, run this callback
+    .then(function(data) {
+      // log the data we found
+      console.log(data);
+      // tell the user we're adding a review with an alert window
+      alert("Adding review...");
+    });
+
+  // empty each input box by replacing the value with an empty string
+  $("#city").val("");
+  $("#hotel").val("");
+  $("#ratings").val("");
+  $("#comments").val("");
+
+});
---
+++ js/view.js
@@ -0,0 +1,39 @@
+// Code here handles queries for specific characters in the database
+// In this case, the user submits a character's name... we then pass that character's name as a
+// URL parameter. Our server then performs the search to grab that character from the Database.
+
+// when user hits the search-btn
+$("#search-btn").on("click", function() {
+  // save the character they typed into the character-search input
+  var searchedHotel = $("#hotel-search")
+    .val()
+    .trim();
+
+  // Using a RegEx Pattern to remove spaces from searchedCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  searchedHotel = searchedHotel.replace(/\s+/g, "").toLowerCase();
+
+  // run an AJAX GET-request for our servers api,
+  // including the user's character in the url
+  $.get("/api/" + searchedHotel, function(data) {
+    // log the data to our console
+    console.log(data);
+    // empty to well-section before adding new content
+    $("#well-section").empty();
+    // if the data is not there, then return an error message
+    if (!data) {
+      $("#well-section").append("<h2> No Review was found </h2>");
+    }
+    else {
+      // otherwise
+      // append the Review name
+      $("#well-section").append("<h2>" + data.city + "</h2>");
+      // the role
+      $("#well-section").append("<h3>Hotel: " + data.hotel + "</h3>");
+      // the age
+      $("#well-section").append("<h3>Ratings: " + data.ratings + "</h3>");
+      // and the force points
+      $("#well-section").append("<h3>Comments: " + data.comments + "</h3>");
+    }
+  });
+});
