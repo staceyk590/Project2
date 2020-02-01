933a3847e9aa2300c40e9cfc13f4ad253753b214
khoingx <55979401+khoingx@users.noreply.github.com>
2020-02-01 16:28:56 +1100

    update jquery and route for search function

--- controllers/hotel-api-routes.js
+++ controllers/hotel-api-routes.js
@@ -1,11 +1,11 @@
 const db = require("../models");
 
 module.exports = function(app) {
-  app.get("/api/Hotel", function(req, res) {
+  app.get("/api/Hotel/", function(req, res) {
     // Here we add an "include" property to our options in our findAll query
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.Post
-    db.Hotel.findAll({
+    db.Hotel.findOne({
       include: [db.Author]
     }).then(function(dbHotel) {
       res.json(dbHotel);
@@ -13,21 +13,49 @@ module.exports = function(app) {
   });
 
   app.get("/api/Hotel/:hotelName", function(req, res) {
+
+    var hotelName = req.params.hotelName;
+    var city = req.params.hotelCity;
+
+    // if(hotelName == null || city == null){
+    //   res.send("Please provide both city and hotel name");
+    //   res.end();
+    // }
     // Here we add an "include" property to our options in our findOne query
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.Post
     db.Hotel.findOne({
       where: {
-<<<<<<< HEAD
-        // id: req.params.id
-        hotelName: req.params.hotelName
-      }
-=======
-        city: req.params.city,
-        name: req.params.name
-        },
-      include: [db.Post]
->>>>>>> 59464cbcc98753a45aed18bbbd6a2442d9dc1fe6
+        // city: req.params.city,
+        hotelName: req.params.hotelName,
+        // hotelCity: req.params.hotelCity
+        }
+      // include: [db.Post]
+    }).then(function(dbHotel) {
+      res.json(dbHotel);
+      console.log(dbHotel);
+    });
+  });
+  
+  app.get("/api/Hotel/:hotelName/:hotelCity", function(req, res) {
+
+    var hotelName = req.params.hotelName;
+    var city = req.params.hotelCity;
+
+    // if(hotelName == null || city == null){
+    //   res.send("Please provide both city and hotel name");
+    //   res.end();
+    // }
+    // Here we add an "include" property to our options in our findOne query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Post
+    db.Hotel.findOne({
+      where: {
+        // city: req.params.city,
+        hotelName: req.params.hotelName,
+        hotelCity: req.params.hotelCity
+        }
+      // include: [db.Post]
     }).then(function(dbHotel) {
       res.json(dbHotel);
       console.log(dbHotel);
